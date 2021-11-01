9de3147bad87738546f913a3e8702d1e214b831a
Julian <juli@santsbastiademontmajor.cat>
2021-11-01 19:32:39 +0100

    changes timzone to Europe/Andorra

--- docker-run.sh
+++ docker-run.sh
@@ -6,11 +6,11 @@ docker run -d --name="Zoneminder" \
 --shm-size="8G" \
 -p 8443:443/tcp \
 -p 9000:9000/tcp \
--e TZ="America/New_York" \
+-e TZ="Europe/Andorra" \
 -e PUID="99" \
 -e PGID="100" \
 -e MULTI_PORT_START="0" \
 -e MULTI_PORT_END="0" \
 -v "/mnt/cache/appdata/Zoneminder":"/config":rw \
 -v "/mnt/cache/appdata/Zoneminder/data":"/var/cache/zoneminder":rw \
-dlandon/zoneminder.machine.learning
+lumpenpancho/zoneminder.machine.learning
